fd5cf9f9c584130e6e45d016eaa1defa56cff7ac
HintikkaKimmo <Hintikkakimmo@gmail.com>
2015-12-18 09:23:33 +0100

    Article editing validation and updating added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -12,6 +16,17 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was succesfully edited"
+    redirect_to article_path(@article)
+  else
+    render 'edit'
+    end
+  end
+
 
   def show
     @article = Article.find(params[:id])
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
