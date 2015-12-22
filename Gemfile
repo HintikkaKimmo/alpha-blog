fa09ac924d7e86def71fbc5c31776f9933a29a61
HintikkaKimmo <Hintikkakimmo@gmail.com>
2015-12-22 10:04:50 +0000

    add paginations to views

--- Gemfile
+++ Gemfile
@@ -25,6 +25,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+# Will paginate gem
+gem 'will_paginate', '~> 3.0', '>= 3.0.7'
+
+# Bootstrap pagination
+gem 'bootstrap-will_paginate', '~> 0.0.10'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -35,6 +35,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
